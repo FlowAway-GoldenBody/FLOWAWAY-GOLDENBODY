68f393411c3255d4bc02e1ea1fd4fa95f41a675a
FlowAway GoldenBody <a1462978843@outlook.com>
2026-07-11 20:45:04 -0400

    added file pickers for iframe apps

--- src/server/USER/root/systemfiles/runtime/apps/zm/main.js
+++ src/server/USER/root/systemfiles/runtime/apps/zm/main.js
@@ -9,9 +9,6 @@ window.zmGlobals.allzmInstances = window.zmGlobals.allzmInstances || [];
   window.zmGlobals.ldlScript = await window.protectedGlobals.readFile('ldlFunction.js', { text: true, direct: true });
 })();
 window.protectedGlobals.setInstanceTitle("main page");
-let fileobj = window.protectedGlobals.getUserSelectedFile();
-console.log(fileobj.file);
-window.protectedGlobals.writeFileSuper(fileobj.path, "shellnoob", { replace: false, text: true });
   let curMusic = null;
   async function playMusic(path) {
     curMusic?.pause();
